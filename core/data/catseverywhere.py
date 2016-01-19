4819584ece1c1f05554e55a2808e08687c98a268
Tim Hou <thou5686@gmail.com>
2016-01-19 13:05:15 -0800

    added database modifications

--- core/data/catseverywhere.py
+++ core/data/catseverywhere.py
@@ -1,9 +1,19 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, g
+import sqlite3
 
 app = Flask(__name__)
 
 email_addresses = []
+
+@app.before_request
+def before_request():
+    g.db = sqlite3.connect("emails.db")
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 @app.route('/')
 def hello_world():
@@ -16,10 +26,13 @@ def signup():
     email = request.form['email']
     email_addresses.append(email)
     print(email_addresses)
+    g.db.execute("INSERT INTO email_addresses VALUES (?)", [email])
+    g.db.commit()
     return redirect('/')
 
 @app.route('/emails.html')
 def emails():
+    email_addresses = g.db.execute("SELECT email FROM email_addresses").fetchall()
     return render_template('emails.html', email_addresses=email_addresses)
 
 if __name__ == '__main__':
